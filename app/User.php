8d5e18d97456972113423533f3a30d04a017d3c9
silversea <like58349@gmail.com>
2020-05-10 01:00:35 +0800

    .

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'LastName','FirstName', 'tel','email', 'password',
+        'user_id','LastName','FirstName', 'tel','email', 'password',
     ];
 
     /**
