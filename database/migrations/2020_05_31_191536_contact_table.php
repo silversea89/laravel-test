2a75dbc3255fcc61d925ba2148bdc050e3d253d7
silversea <like58349@gmail.com>
2020-06-01 04:24:00 +0800

    error

---
+++ database/migrations/2020_05_31_191536_contact_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ContactTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact', function (Blueprint $table) {
+            $table->id();
+            $table->string('user');
+            $table->string('email');
+            $table->string('title');
+            $table->string('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contact');
+    }
+}
