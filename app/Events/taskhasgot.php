21c39cfc20d5b32d19c5d33a29d33219dc026a34
2606 <like58349@gmail.com>
2020-09-25 20:25:35 +0800

    notification

--- app/Events/taskhasgot.php
+++ app/Events/taskhasgot.php
@@ -2,6 +2,8 @@
 
 namespace App\Events;
 
+use App\Notification;
+use Carbon\Carbon;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
@@ -11,22 +13,30 @@ class taskhasgot implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
+    public $from;
     public $username;
-
     public $message;
-
     public $target;
+    public $time;
 
     /**
      * Create a new event instance.
      *
      * @return void
      */
-    public function __construct($username)
+    public function __construct($from, $target)
     {
-        $this->username = $username;
+        $username = $from->name;
         $this->message  = "{$username} 接受了你的委託";
-//        $this->target = $target;
+        $this->target = $target;
+        $this->time=Carbon::now();
+        Notification::create([
+            'from' => $from->student_id,
+            'to' => $target,
+            'message' => $this->message,
+            'read' => false,
+            'created_at' => $this->time
+        ]);
     }
 
     /**
@@ -36,7 +46,6 @@ class taskhasgot implements ShouldBroadcast
      */
     public function broadcastOn()
     {
-//        return ['taskhasgot.'.$this->target];
-        return ["status-liked"];
+        return ['taskhasgot.' . $this->target];
     }
 }
